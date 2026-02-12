a2b697327eb1249938f7cf77fe4b87c8955b41cd
Omar <Omar@DESKTOP-5U4L7UF>
2026-02-13 01:16:06 +0200

    Add project files.

---
+++ G-NET-17-C#-5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G_NET_17_C__5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
